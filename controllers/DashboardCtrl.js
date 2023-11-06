39daf228de886869e72d15e7c98be8827e47ef93
Dennis Klarenbeek <dennisklarenbeek@DenBook-Pro.local>
2023-11-06 11:53:15 +0100

    added week target

--- controllers/DashboardCtrl.js
+++ controllers/DashboardCtrl.js
@@ -4,8 +4,23 @@ const { formatCurrency } = require("../utils/formatting");
 
 const targetQ4 = 3880021;
 
+const calculateWeekOrderIntake = (orders, weeknumber) => {
+    const value = orders.reduce((total, current) => {
+        const orderWeek = moment(current.date).week();
+
+        if (weeknumber === orderWeek) {
+            return total + current.value;
+        } else {
+            return total;
+        }
+    }, 0);
+    return value;
+};
+
 exports.dashboard = async (req, res) => {
     const thisWeek = moment().week();
+    const lastWeek = thisWeek - 1;
+    const weekEndYear = 51;
     const orders = await Order.find();
     const totalValue = orders.reduce((total, current) => {
         return total + current.value;
@@ -22,15 +37,11 @@ exports.dashboard = async (req, res) => {
 
     const forecastDelta = targetQ4 - q4Intake;
 
-    const orderValueThisWeek = orders.reduce((total, current) => {
-        const orderWeek = moment(current.date).week();
+    const orderValueThisWeek = calculateWeekOrderIntake(orders, thisWeek);
+    const orderValueLastWeek = calculateWeekOrderIntake(orders, lastWeek);
 
-        if (thisWeek === orderWeek) {
-            return total + current.value;
-        } else {
-            return total;
-        }
-    }, 0);
+    const numberOfWeeksLeft = weekEndYear + 1 - thisWeek; // Add one week to make sure the actual week is also calculated
+    const weekTargetOrderIntake = forecastDelta / numberOfWeeksLeft;
 
     res.render("dashboard", {
         total: formatCurrency(totalValue),
@@ -38,5 +49,7 @@ exports.dashboard = async (req, res) => {
         intakeQ4: formatCurrency(q4Intake),
         delta: formatCurrency(forecastDelta),
         thisWeek: formatCurrency(orderValueThisWeek),
+        lastWeek: formatCurrency(orderValueLastWeek),
+        weektarget: formatCurrency(weekTargetOrderIntake),
     });
 };
